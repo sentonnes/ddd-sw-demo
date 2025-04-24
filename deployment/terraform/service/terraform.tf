a6ea847a2190092f0df5b86206ab4830ae29239c
Sen <scottjames2008@hotmail.co.uk>
2025-04-24 19:08:24 +0100

    Add Terraform with Pipeline

---
+++ deployment/terraform/service/terraform.tf
@@ -0,0 +1,55 @@
+﻿provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.app_name+"-rg"
+  location = "UK South"
+}
+
+resource "azurerm_app_service_plan" "plan" {
+  name                = var.app_name+"-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_linux_web_app" "webapp" {
+  name                = var.app_name+"-webapp"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_app_service_plan.plan.id
+
+  site_config {
+    always_on        = true
+    linux_fx_version = "DOTNET|8.0"
+  }
+
+  app_settings = {
+    "WEBSITE_RUN_FROM_PACKAGE" = "1"
+  }
+}
+
+resource "azurerm_linux_web_app_slot" "slot" {
+  name           = var.app_name+"-staging"
+  app_service_id = azurerm_linux_web_app.webapp.id
+
+  site_config {
+    always_on        = true
+    linux_fx_version = "DOTNET|8.0"
+  }
+
+  app_settings = {
+    "WEBSITE_RUN_FROM_PACKAGE" = "1"
+  }
+}
+
+variable "app_name" {
+  type = string
+}
