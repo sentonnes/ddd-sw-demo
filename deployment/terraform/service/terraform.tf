2f44fa26e88aaa2151b3cd0139e350915265d01c
Sen <scottjames2008@hotmail.co.uk>
2025-04-24 22:18:34 +0100

    Fix terraform with correct code

--- deployment/terraform/service/terraform.tf
+++ deployment/terraform/service/terraform.tf
@@ -3,12 +3,12 @@
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = var.app_name+"-rg"
+  name     = "${var.app_name}-rg"
   location = "UK South"
 }
 
 resource "azurerm_app_service_plan" "plan" {
-  name                = var.app_name+"-plan"
+  name                = "${var.app_name}-plan"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   kind                = "Linux"
@@ -21,14 +21,13 @@ resource "azurerm_app_service_plan" "plan" {
 }
 
 resource "azurerm_linux_web_app" "webapp" {
-  name                = var.app_name+"-webapp"
+  name                = "${var.app_name}_webapp"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_app_service_plan.plan.id
 
   site_config {
     always_on        = true
-    linux_fx_version = "DOTNET|8.0"
   }
 
   app_settings = {
@@ -37,12 +36,11 @@ resource "azurerm_linux_web_app" "webapp" {
 }
 
 resource "azurerm_linux_web_app_slot" "slot" {
-  name           = var.app_name+"-staging"
+  name           = "${var.app_name}-staging"
   app_service_id = azurerm_linux_web_app.webapp.id
 
   site_config {
     always_on        = true
-    linux_fx_version = "DOTNET|8.0"
   }
 
   app_settings = {
