b5013a878afed758e7d68aa3e12c6c415fbeac0c
Sen <scottjames2008@hotmail.co.uk>
2025-04-28 20:26:34 +0100

    Upgrade terraform to azurerm_service_plan

--- deployment/terraform/service/webapp.tf
+++ deployment/terraform/service/webapp.tf
@@ -7,24 +7,22 @@ resource "azurerm_resource_group" "rg" {
   location = "UK South"
 }
 
-resource "azurerm_app_service_plan" "plan" {
+resource "azurerm_service_plan" "plan" {
   name                = "${local.service_name}-plan"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   kind                = "Linux"
   reserved            = true
 
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
+  os_type  = "Linux"
+  sku_name = "B1"
 }
 
 resource "azurerm_linux_web_app" "webapp" {
   name                = "${local.service_name}_webapp"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id     = azurerm_app_service_plan.plan.id
+  service_plan_id     = azurerm_service_plan.plan.id
 
   site_config {
     always_on        = true
